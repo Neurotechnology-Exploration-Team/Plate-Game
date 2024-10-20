b32a8f0f74763748633ea99515ec46b5914ceb0c
Ethan-Caracoglia <ethancaracoglia@gmail.com>
2024-10-20 16:25:52 -0400

    Ball and Move updated. Ball now interacts diffrerently with the player collision than it does with the wall collision. This is works more like Breakout now.

--- neurotech_plate_game/Assets/Scripts/Ball.cs
+++ neurotech_plate_game/Assets/Scripts/Ball.cs
@@ -1,26 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
+/// <summary>
+/// A ball that bounces off of the player block in a direction that changes based off where the 
+/// ball hits the player relative to its center.
+/// </summary>
 public class Ball : MonoBehaviour
 {
-    public Rigidbody2D Rigidbody;
-    public float speed = 500f;
+    #region Fields
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private Collider2D col;
+    private Vector3 velocity;
+    private Vector3 direction;
+    private Vector3 position;
+    private float angle;
+    private bool paused; // When we implement pausing this should be used
+    #endregion
 
-
-    // Start is called before the first frame update
+    #region Methods
+    #region Internal Methods
     void Start()
     {
-        this.Rigidbody = GetComponent<Rigidbody2D>();
+        paused = false;
         Invoke(nameof(SetRandomTrajectory), 1f);
+        position = transform.position;
     }
 
+    void Update()
+    {
+        if (!paused)
+        {
+            // Scale the velocity based on the deltaTime (time between current and last frame)
+            velocity = direction * speed * Time.deltaTime;
+            position += velocity;
+            transform.position = position;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject != player)
+        {
+            // Check which direction the ball hit from
+
+            // the normal is the vector that is perpendicular to the side of the collider
+            // that was hit, so if the top is hit then that will be the up vector.
+
+            // This will immediately stop working if any thing is rotated
+            Vector3 norm = collision.contacts[0].normal;
+            if (norm == Vector3.up)
+            {
+                direction.y *= -1;
+            }
+            else if (norm == Vector3.down)
+            {
+                direction.y *= -1;
+            }
+            if (norm == Vector3.right)
+            {
+                direction.x *= -1;
+            }
+            else if (norm == Vector3.left)
+            {
+                direction.x *= -1;
+            }
+        }
+    }
+    #endregion
+
+    #region External Methods
+    /// <summary>
+    /// Used to start the ball at a random position and trajectory towards the player.
+    /// </summary>
     public void SetRandomTrajectory()
     {
-        Vector2 force = Vector2.zero;
-        force.x = Random.Range(-1f, 1f);
-        force.y = -1f;
-
-        this.Rigidbody.AddForce(force.normalized * this.speed);
+        float randAngle = Random.Range(Mathf.PI + 1, 2 * Mathf.PI - 1);   
+        direction = new Vector3(Mathf.Cos(randAngle), Mathf.Sin(randAngle));
     }
+
+    /// <summary>
+    /// Reads the normalized distance from the center of player and sends the ball in a direction
+    /// away from the player with the angle of the direction equal to the arccos of the n-distance
+    /// </summary>
+    /// <param name="dist">The normalized horizontal distance from the center of the player</param>
+    public void PlayerBounce(float dist)
+    {
+        // If the ball hits the side it will return a value greater than 1 or less than -1 because
+        // the center of the ball is outside the horizontal extent of the player block. Reverse
+        // x when this is the case otherwise it will throw a NaN for arccos(dist)
+        if (dist >= 1 || dist <= -1)
+        {
+            direction.x *= -1f;
+        }
+        // Send the ball straight up if the distance is zero
+        else if (dist == 0)
+        {
+            direction = Vector3.up;
+        }
+        // Sends the at an angle according to the balls horizontal distance from the center of the
+        // player on impact
+        else
+        {
+            Debug.Log(dist);
+            angle = Mathf.Acos(dist);
+            direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle));
+        }
+    }
+    #endregion
+    #endregion
 }
--- neurotech_plate_game/Assets/Scripts/Move.cs
+++ neurotech_plate_game/Assets/Scripts/Move.cs
@@ -4,26 +4,47 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Move : MonoBehaviour
 {
-    public InputActionAsset controls;
-    [SerializeField] float SPEED = 0.1f;
-    bool holdMoveRight = false;
-    bool holdMoveLeft = false;
+    #region Fields
+    [SerializeField] private GameObject ball;
+    [SerializeField] private Ball ballScript;
+    [SerializeField] private SpriteRenderer spr;
+    [SerializeField] private float speed = 5f;
+    private bool holdMoveRight = false;
+    private bool holdMoveLeft = false;
+    #endregion
+
+    #region Methods
+    #region Internal Methods
     private void Update()
     {
         if (holdMoveRight)
         {
-            gameObject.transform.position += new Vector3(SPEED, 0);
+            gameObject.transform.position += new Vector3(speed * Time.deltaTime, 0);
         }
 
         if (holdMoveLeft)
         {
-            gameObject.transform.position += new Vector3(-SPEED, 0);
+            gameObject.transform.position += new Vector3(-speed * Time.deltaTime, 0);
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If the collided object was the ball then trigger it's player bounce
+        if (collision.gameObject == ball)
+        {
+            float dist = collision.transform.position.x - transform.position.x;
+            dist *= (1 / spr.bounds.extents.x); // dist is normalized here so that the horizontal point furthest away from the center is now equal to 1 (or -1 on the left)
+            ballScript.PlayerBounce(dist);
+        }
+    }
+    #endregion
+
+    #region External Methods
     public void MoveRight(InputAction.CallbackContext ctx)
     {
         if (ctx.performed) holdMoveRight = true;
@@ -35,4 +56,6 @@ public class Move : MonoBehaviour
         if (ctx.performed) holdMoveLeft = true;
         else if (ctx.canceled) holdMoveLeft = false;
     }
+    #endregion
+    #endregion
 }
